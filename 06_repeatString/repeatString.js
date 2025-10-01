245d21797e7ca821dd3b5f912c26073bc716515b
Hikaru <122536382+hikaru5426@users.noreply.github.com>
2025-10-01 16:43:37 +0200

    Exo 6 JS done

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, repeatedNumber) {
 
+    if (repeatedNumber < 0){
+        return "ERROR";
+    }
+
+    result = "";
+    for (counter = 1; counter <= repeatedNumber; counter++){
+        result += string;
+    }
+    
+    return result;
 };
 
 // Do not edit below this line
