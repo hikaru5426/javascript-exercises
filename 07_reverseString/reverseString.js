b81551e2aadf5c7b55bae412d11f2487f0ac3428
Hikaru <122536382+hikaru5426@users.noreply.github.com>
2025-10-14 12:29:10 +0200

    Exo 07 done

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = "";
+    for(character = string.length - 1; character >= 0; character --){
+        reversedString += string[character];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
