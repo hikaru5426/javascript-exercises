48363baa8de4d40d12feaa5f333441bf681e3cb5
Hikaru <122536382+hikaru5426@users.noreply.github.com>
2025-10-14 13:01:52 +0200

    Exo 10 done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    const aBigger = a > b ? true : false;
+    let sum = 0;
+    if(a < 0 || b < 0 || typeof a !== "number" || typeof b !== "number" || !Number.isInteger(a) || !Number.isInteger(b)){
+        return "ERROR";
+    }else if(aBigger){
+        for (integer = b; integer <= a; integer++){
+            sum += integer;
+        }
+    } else{
+        for (integer = a; integer <= b; integer++){
+            sum += integer;
+        }
+    }
+    return a === b ? a  * 2 : sum;
 };
 
 // Do not edit below this line
