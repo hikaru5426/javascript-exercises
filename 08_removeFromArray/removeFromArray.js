b90759782064a6e6e02abff87fee4b034b52f0d4
Hikaru <122536382+hikaru5426@users.noreply.github.com>
2025-10-14 12:36:38 +0200

    Exo 08 done

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elementToRemove) {
+    return arr.filter((element) => !elementToRemove.includes(element));
 };
 
 // Do not edit below this line
