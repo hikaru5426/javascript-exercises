423e819cd2f15cc3b854d985a31790f5a06d8199
Hikaru <122536382+hikaru5426@users.noreply.github.com>
2025-10-20 21:30:51 +0200

    Exo 12 done

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((accumulator, number) => accumulator + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  if(array.length === 0){
+    return 0
+  }
+  let result = 1;
+  array.map((element) => result *= element);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base**exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let result = 1;
+	for (number = 2; number <= n; number ++){
+    result *= number;
+  }
+  return result;
 };
 
 // Do not edit below this line
