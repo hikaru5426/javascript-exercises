f48284d4322450064266b88e6428d812fbac9eae
Hikaru <122536382+hikaru5426@users.noreply.github.com>
2025-10-21 00:23:20 +0200

    Exo 13 done

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    stringCleared = string
+        .trim()
+        .toLowerCase()
+        .split("");
+    const authorizedCharacters = [
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+        "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+        "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+    ];
+    stringCleared = stringCleared.filter((character) => authorizedCharacters.includes(character));
+    return (stringCleared.join("") === [...stringCleared].reverse().join(""));
 };
 
 // Do not edit below this line
