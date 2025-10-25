fe1da742536b356363016dce83625931e7d18569
Hikaru <122536382+hikaru5426@users.noreply.github.com>
2025-10-25 18:23:47 +0200

    Exo 16 done

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    let now = new Date();
+    let oldestPersonAge;
+    let currentPersonAge;
+    let oldestPerson = {
+        name: "",
+        yearOfBirth: 0,
+        yearOfDeath: 0,
+    };
 
+    for (let person of people) {
+        
+        if (person.yearOfDeath === undefined) {
+            currentPersonAge = now.getFullYear() - person.yearOfBirth;
+        } else {
+            currentPersonAge = person.yearOfDeath - person.yearOfBirth;
+        }
+
+        if (oldestPerson.yearOfDeath === undefined) {
+            oldestPersonAge = now.getFullYear() - oldestPerson.yearOfBirth;
+        } else {
+            oldestPersonAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+        }
+
+        if (currentPersonAge > oldestPersonAge) {
+            oldestPerson = person;
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
