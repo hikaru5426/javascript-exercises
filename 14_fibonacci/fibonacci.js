9bdab8fb51b771c9c923c227446a3c8da54d6c98
Hikaru <122536382+hikaru5426@users.noreply.github.com>
2025-10-25 17:33:03 +0200

    Exo 14 done

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let fibonacci = [1, 1];
+    let numberParsed = Number(number);
+
+    if (numberParsed < 0){
+        return "OOPS";
+    }
+    if(numberParsed == 0){
+        return 0;
+    }
+
+    for(let i = 2; i < numberParsed; i++){
+        fibonacci.push(fibonacci[i-2]+fibonacci[i-1]);
+    }
+
+    return fibonacci[numberParsed-1];
 
 };
 
